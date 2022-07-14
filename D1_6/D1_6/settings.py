2fb98721da52268a83e6d40cc577ac9812dbcad7
baritone1970 <baritone@inbox.ru>
2022-07-14 22:59:11 +0300

    Всё готово, хотя и бестолково

--- D1_6/D1_6/settings.py
+++ D1_6/D1_6/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # подключаем ещё приложения
-    'django.contrib.sites', #  sites framework
+    'django.contrib.sites',  #  sites framework
     'django.contrib.flatpages',
 ]
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'D1_6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
